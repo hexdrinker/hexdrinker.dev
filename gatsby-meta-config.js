9b0dec5c5ee0ed8afc51080c60e352ab4f0fba80
박영호 <hexdrinker2020@gmail.com>
2021-04-15 16:09:56 +0900

    블로그 정보 업데이트

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `hexdrinker's DevLog`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  author: `hexdrinker`,
+  introduction: `Software Engineer at Backpackr`,
+  siteUrl: `https://hexdrinker.dev`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `hexdrinker`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
-    instagram: ``, // Your Instagram account
+    facebook: `hexdrinker2020`, // Your Facebook account
+    linkedin: `hexdrinker`, // Your LinkedIn account
+    instagram: `hexxdrinker`, // Your Instagram account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'hexdrinker/hexdrinker.dev', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
