a86d2f854d1f3334b3e1da5306860f9d58033dcc
박영호 <hexdrinker2020@gmail.com>
2021-04-16 17:54:28 +0900

    포스트 아이템 코드 수정

--- src/components/post-item/index.jsx
+++ src/components/post-item/index.jsx
@@ -4,26 +4,30 @@ import { TARGET_CLASS } from '../../utils/visible'
 
 import './index.scss'
 
-export const PostItem = ({ node }) => (
-  <div className={`post ${TARGET_CLASS}`} key={node.fields.slug}>
-    {node.frontmatter.thumbnail &&
-      <Link className="post-head" to={node.fields.slug}>
-        <img className="post-thumbnail" src={node.frontmatter.thumbnail} alt="thumbnail image"/>      
-      </Link>
-    }
-    <div className="post-body">
-      <Link to={node.fields.slug}>
-        <h3 className="post-title">{node.frontmatter.title || node.fields.slug}</h3>
-        <p className="post-description">{node.frontmatter.description}</p>
-      </Link>
-      <span className="post-date">{node.frontmatter.date}</span>
-    </div>
-    {node.frontmatter.tags.length ? (
-      <div className="post-footer">
-        {node.frontmatter.tags.map((tag, index) => (
-          <div className="tag" key={`tag_${index}`}>{tag}</div>
-        ))}
+export const PostItem = ({ node }) => {
+  const { title, date, thumbnail, description, tags } = node.frontmatter;
+
+  return (
+    <div className={`post ${TARGET_CLASS}`} key={node.fields.slug}>
+      {node.frontmatter.thumbnail &&
+        <Link className="post-head" to={node.fields.slug}>
+          <img className="post-thumbnail" src={thumbnail} alt="thumbnail image"/>      
+        </Link>
+      }
+      <div className="post-body">
+        <Link to={node.fields.slug}>
+          <h3 className="post-title">{title || node.fields.slug}</h3>
+          {!!description && <p className="post-description">{description}</p>}
+        </Link>
+        <span className="post-date">{date}</span>
       </div>
-    ) : null}
-  </div>
-)
+      {!!tags && (
+        <div className="post-footer">
+          {tags.map((tag, index) => (
+            <div className="tag" key={`tag_${index}`}>{tag}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
