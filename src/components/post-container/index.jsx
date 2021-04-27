fbfb4602a39fdcb034989b32ce1b0565aac07c52
박영호 <hexdrinker2020@gmail.com>
2021-04-27 17:30:19 +0900

    포스트 내 이미지 정렬 및 크기 조절

--- src/components/post-container/index.jsx
+++ src/components/post-container/index.jsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './index.scss'
 
-export const PostContainer = ({ html }) => (
-  <div className="post-container" dangerouslySetInnerHTML={{ __html: html }} />
-)
+export const PostContainer = ({ html }) => {
+  useEffect(() => {
+    const images = document.querySelectorAll('.post-container img')
+    images.forEach(image => {
+      image.parentNode.style.display = "flex"
+      image.parentNode.style.justifyContent = "center"
+    })
+  }, []);
+
+  return <div className="post-container" dangerouslySetInnerHTML={{ __html: html }} />
+}
