6467518e4ba56332f8a5fbe9f8bacf0663a4b5d8
박영호 <hexdrinker2020@gmail.com>
2021-04-28 18:35:55 +0900

    이미지 중앙 정렬 코드 주석처리

--- src/components/post-container/index.jsx
+++ src/components/post-container/index.jsx
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react'
 import './index.scss'
 
 export const PostContainer = ({ html }) => {
-  useEffect(() => {
-    const images = document.querySelectorAll('.post-container img')
-    images.forEach(image => {
-      image.parentNode.style.display = "flex"
-      image.parentNode.style.justifyContent = "center"
-    })
-  }, []);
+  // useEffect(() => {
+  //   const images = document.querySelectorAll('.post-container img')
+  //   images.forEach(image => {
+  //     image.parentNode.style.display = "flex"
+  //     image.parentNode.style.justifyContent = "center"
+  //   })
+  // }, []);
 
   return <div className="post-container" dangerouslySetInnerHTML={{ __html: html }} />
 }
