6b47747c869076fea79d770779c5076d499b36fa
박영호 <hexdrinker2020@gmail.com>
2021-05-01 08:00:29 +0900

    404 노출 문제 해결 시도 1

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-
 import { Layout } from '../layout'
 import { Head } from '../components/head'
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+const browser = typeof window !== "undefined" && window;
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <Head title="404: Not Found" />
-        <h1>404 Not Found</h1>
-        <p>유감스럽지만,,, 존재하지 않는 페이지입니다,,,</p>
-      </Layout>
-    )
-  }
+const NotFoundPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return browser && (
+    <Layout location={location} title={siteTitle}>
+      <Head title="404: Not Found" />
+      <h1>404 Not Found</h1>
+      <p>유감스럽지만,,, 존재하지 않는 페이지입니다,,,</p>
+    </Layout>
+  )
 }
 
 export default NotFoundPage
